3f9308dd239aabacb958aa32b460650bae5b3383
Pavel Tvrdik <pawel.tvrdik@gmail.cz>
2014-12-05 09:25:38 +0100

    BIRD is now easily compileable out of box with GCC at Linux based systems and with clang at BSD based systems.

--- lib/lists.h
+++ lib/lists.h
@@ -50,20 +50,19 @@ typedef struct list {			/* In fact two overlayed nodes */
 
 #define EMPTY_LIST(list) (!(list).head->next)
 
-void add_tail(list *, node *);
-void add_head(list *, node *);
-void rem_node(node *);
-void rem2_node(node *);
-void add_tail_list(list *, list *);
-void init_list(list *);
-void insert_node(node *, node *);
-
 #ifndef _BIRD_LISTS_C_
-#define LIST_INLINE extern inline
-#include "lib/lists.c"
+#define LIST_INLINE static inline
+  #include "lib/lists.c"
 #undef LIST_INLINE
 #else
 #define LIST_INLINE
+  extern void add_tail(list *, node *);
+  extern void add_head(list *, node *);
+  extern void rem_node(node *);
+  extern void rem2_node(node *);
+  extern void add_tail_list(list *, list *);
+  extern void init_list(list *);
+  extern void insert_node(node *, node *);
 #endif
 
 #endif
